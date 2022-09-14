3e5bafc61394ff4b5dffb9ca089467fb363131c8
Taneli <13731577+tlzdev@users.noreply.github>
2022-09-14 22:22:36 +0300

    feat: cleanup and refactor _app.js session fetching. add prettier config

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,11 @@
-import "../styles/globals.css";
+import '../styles/globals.css';
 
-import App from "next/app";
+import App from 'next/app';
 
-import SessionContext from "../utils/SessionContext";
+import SessionContext from '../utils/SessionContext';
 
 const AppProvider = ({ children, initialData = {} }) => {
-  return (
-    <SessionContext.Provider value={initialData}>
-      {children}
-    </SessionContext.Provider>
-  );
+  return <SessionContext.Provider value={initialData}>{children}</SessionContext.Provider>;
 };
 
 const MyApp = ({ Component, pageProps }) => {
@@ -20,54 +16,66 @@ const MyApp = ({ Component, pageProps }) => {
   );
 };
 
+const fetchOrySessionFromCookie = async (cookie) => {
+  if (!cookie) {
+    throw Error('fetchOrySessionFromCookie util function called without a cookie');
+  }
+
+  // Fetch session details in backend using Ory API
+
+  const sessionRequest = await fetch('http://localhost:3000/api/.ory/sessions/whoami', {
+    headers: {
+      Cookie: cookie,
+    },
+  });
+
+  if (sessionRequest.status !== 200) {
+    throw Error('Failed to fetch session from Ory');
+  }
+
+  const orySessionResponse = await sessionRequest.json();
+
+  return orySessionResponse;
+};
+
 MyApp.getInitialProps = async (appContext) => {
   const appProps = await App.getInitialProps(appContext);
+
+  // if browser, skip
   if (process.browser) {
     return __NEXT_DATA__.props.pageProps;
   }
+
+  // if server, cookies from client are used to fetch Ory session details from API
   const cookies = appContext.ctx.req.cookies;
+
   if (cookies && !!Object.entries(cookies)) {
     try {
-      // Use clients session cookie to fetch session serverside
-      const objectToCookie = Object.entries(cookies)
-        .filter(([key, value]) => key.includes("ory_session"))
-        .map(([key, value]) => [key, value].join("="))
-        .join("; ");
+      // cookie starting with ory_session_ is the correct one. cookie name depends on used ory instance name.
+      const cookie = Object.entries(cookies)
+        .filter(([key, value]) => key.includes('ory_session'))
+        .map(([key, value]) => [key, value].join('='))
+        .join('; ');
 
-      // Some cookies exist, but no session cookie => return null session
-      if (!objectToCookie) {
+      // If some cookies exist, but no session cookie => return null session
+      if (!cookie) {
         return { ...appProps, contextData: { session: null } };
       }
 
-      // Fetch session details in backend using Ory API
+      const session = await fetchOrySessionFromCookie(cookie);
 
-      const sessionRequest = await fetch(
-        "http://localhost:3000/api/.ory/sessions/whoami",
-        {
-          headers: {
-            Cookie: objectToCookie,
-          },
-        }
-      );
-
-      if (sessionRequest.status !== 200) {
-        throw Error("Failed to fetch session from Ory");
-      }
-
-      const response = await sessionRequest.json();
-
-      // Return session details to be used in UI
+      // Return session details back to client to be used in UI
       return {
         ...appProps,
         pageProps: {
           ...appProps?.pageProps,
           contextData: {
-            session: response,
+            session,
           },
         },
       };
     } catch (error) {
-      console.error("Failed to fetch Ory session.");
+      console.error('Failed to fetch Ory session.');
       console.error(error);
     }
   }
